25b41632c34824c977da8089f243c1ac9e8a12bb
Cristopher Andrés Sáez Araneda <cristopher.saez2001@alumnos.ubiobio.cl>
2024-03-16 18:10:56 -0300

    Realizado ejercicio 1

---
+++ Variables y Tipos de Datos/1.js
@@ -0,0 +1,12 @@
+//Declara una variable llamada nombre y asígnale un valor de tipo string.
+const nombre = "Cristopher";
+console.log(nombre)
+console.log(typeof nombre)
+//Declara una variable llamada edad y asígnale un valor de tipo number.
+var edad = 22;
+console.log(edad)
+console.log(typeof edad)
+//Declara una variable llamada puedoConducir y asígnale un valor de tipo boolean .
+var puedoConducir = true; 
+console.log(puedoConducir)
+console.log(typeof puedoConducir)
