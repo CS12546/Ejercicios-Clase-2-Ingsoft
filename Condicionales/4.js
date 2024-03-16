7c13215b042c9219eea139be89d3aa52b8ced80c
Cristopher Andrés Sáez Araneda <cristopher.saez2001@alumnos.ubiobio.cl>
2024-03-16 18:27:27 -0300

    ejercicios 4 y 5

---
+++ Condicionales/4.js
@@ -0,0 +1,28 @@
+// Dados los siguientes valores numéricos
+const numero1 = 10;
+const numero2 = 20;
+const numero3 = 30;
+
+// 1. Imprimir en consola el número mayor de los tres.
+console.log("El número mayor es:", Math.max(numero1, numero2, numero3));
+
+// 2. Imprimir en consola el número menor de los tres.
+console.log("El número menor es:", Math.min(numero1, numero2, numero3));
+
+// 3. Imprimir en consola si el numero1 es par o impar.
+console.log("numero1 es", numero1 % 2 === 0 ? "par" : "impar");
+
+// 4. Imprimir en consola si el numero2 es par o impar.
+console.log("numero2 es", numero2 % 2 === 0 ? "par" : "impar");
+
+// 5. Imprimir en consola si el numero3 es par o impar.
+console.log("numero3 es", numero3 % 2 === 0 ? "par" : "impar");
+
+// 6. Imprimir en consola si el numero1 es múltiplo de 5.
+console.log("numero1 es múltiplo de 5:", numero1 % 5 === 0);
+
+// 7. Imprimir en consola si el numero2 es múltiplo de 5.
+console.log("numero2 es múltiplo de 5:", numero2 % 5 === 0);
+
+// 8. Imprimir en consola si el numero3 es múltiplo de 5.
+console.log("numero3 es múltiplo de 5:", numero3 % 5 === 0);
