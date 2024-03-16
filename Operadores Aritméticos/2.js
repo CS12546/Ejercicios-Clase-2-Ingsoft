2ef0f7c11056f12657a6decb8fb2cf155c4b23af
Cristopher Andrés Sáez Araneda <cristopher.saez2001@alumnos.ubiobio.cl>
2024-03-16 18:18:31 -0300

    Ejercicio 2

---
+++ Operadores Aritméticos/2.js
@@ -0,0 +1,27 @@
+//Dado los siguiente valores numéricos:
+const numero1 = 20;
+const numero2 = 40;
+
+//Suma (+): Suma numero1 y numero2. Muestra el resultado por consola.
+const Suma = numero1 + numero2;
+console.log(Suma)
+
+//Resta (-): Resta numero1 y numero2. Muestra el resultado por consola.
+
+const Resta = numero1 - numero2;
+console.log(Resta)
+
+//Multiplicación (*): Multiplica numero1 y numero2. Muestra el resultado por consola.
+
+const Multiplicación = numero1 * numero2;
+console.log(Multiplicación)
+
+//División (/): Divide numero1 y numero2. Muestra el resultado por consola.
+
+const División = numero1 / numero2;
+console.log(División)
+
+//Modulo (%):  Saca el modulo entre numero2 y numero1. Muestra el resultado por consola.
+
+const Modulo = numero1 % numero2;
+console.log(Modulo)
